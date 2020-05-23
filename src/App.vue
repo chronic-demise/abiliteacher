134bc18f09f10168104b62ef35f06ed4adcac10a
Ryan Norby <ryan@berserkguard.com>
2020-05-23 04:08:13 -0400

    Ripped out hello world code and began building an Actionbar component.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Actionbar
+            :config="config"
+            :numIcons="8"
+            :abilities="['corruption_shot', 'needle_strike']" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Actionbar from './components/Actionbar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Actionbar
+    },
+    data: function() {
+        return {
+            "config": {
+                "corruption_shot": {
+                    "icon": "icons/corruption_shot.webp",
+                },
+                "needle_strike": {
+                    "icon": "icons/needle_strike.webp",
+                },
+                "empty_slot": {
+                    "icon": "icons/empty_slot.png",
+                }
+            }
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Actionbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="actionbar">
+        <div v-for="item in numIcons" v-bind:key="item">
+            <div v-if="abilities[item - 1] != null">
+                <img class="ability_icon" v-bind:src="config[abilities[item - 1]].icon" />
+            </div>
+            <div v-else>
+                <img class="ability_icon" v-bind:src="config.empty_slot.icon" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Actionbar',
+    props: {
+        numIcons: Number,
+        abilities: Array,
+        config: Object,
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ability_icon {
+    width: 36px;
+    height: 36px;
+    float: left;
+    padding: 2px;
+}
+
+.actionbar:after {
+    display: block;
+    content: "";
+    clear: both;
+}
+</style>
